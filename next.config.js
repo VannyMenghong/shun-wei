00502050d887af37e0a01247a95dedcaa67c5237
menghong <vanny.menghong@outlook.com>
2023-09-10 18:09:06 +0700

    Deploying to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: "/shun-wei",
+  assetPrefix: "/shun-wei",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
