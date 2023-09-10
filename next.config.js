64a38508302dc1977750e96ef6d455bbb3b788fc
menghong <vanny.menghong@outlook.com>
2023-09-10 19:04:57 +0700

    u

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/shun-wei",
-  assetPrefix: "/shun-wei",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
